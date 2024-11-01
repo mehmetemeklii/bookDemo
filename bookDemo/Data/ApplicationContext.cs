cb24f580846fae7aca752f66b3075d6bd639f970
mehmetemeklii <mehmetemeklii@hotmail.com>
2024-11-01 21:24:57 +0300

    Proje dosyası ekle.

---
+++ bookDemo/Data/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; } 
+
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book() {Id = 1,Title="Hacıvat ve Karagöz", Price=75},
+                new Book() {Id = 2,Title="Mesnevi", Price=150},
+                new Book() {Id = 3,Title="Dede Korkut", Price=125}
+            };
+        }
+    }
+}
---
+++ bookDemo/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace bookDemo.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public int Price { get; set; }
+
+    }
+}
